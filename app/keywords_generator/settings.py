6e92c6625b2a42ecb87ddf19e03cb817a9b4f91a
Kurt Paguio <kurt.paguio.simpletrade@gmail.com>
2024-11-06 23:05:01 +0800

    Set debug to false

--- app/keywords_generator/settings.py
+++ app/keywords_generator/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8_0l_(qwjjm+4mk%u&amg@jca8vhl2%co_wz9i76jzu-wev01!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
