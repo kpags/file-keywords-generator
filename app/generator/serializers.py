c4e5d9f2f9b89bf3335559a59a085d28138acec1
Kurt Paguio <kurt.paguio.simpletrade@gmail.com>
2024-11-07 12:54:41 +0800

    Added validations for checking if file is PDF or not

--- app/generator/serializers.py
+++ app/generator/serializers.py
@@ -3,3 +3,14 @@ from rest_framework.exceptions import ValidationError
 
 class FileSerializer(serializers.Serializer):
     file = serializers.FileField()
+    
+    def validate(self, attrs):
+        file = attrs.get('file', None)
+        
+        if file is None:
+            raise ValidationError({"message": "PDF File is missing."})
+        
+        if "pdf" not in str(file):
+            raise ValidationError({"message": "Only PDF files are accepted."})
+        
+        return super().validate(attrs)
